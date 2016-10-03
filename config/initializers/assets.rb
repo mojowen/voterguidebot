52408a2e64684b062b6f2f16e44aaae72af774df
Scott Duncombe <srduncombe@gmail.com>
2016-10-03 14:35:07 -0700

    Forgot avg_img

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w{guide.css mail.css
                                                  avg.css avg.js
                                                  avg_embed.js avg_widget.js
+                                                 avg_img.css
                                                  emoji.css emoji.min.js}
