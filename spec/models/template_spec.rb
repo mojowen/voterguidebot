a1699b0eba20318f98a1557d8c38f9b7427e92f7
Scott Duncombe <srduncombe@gmail.com>
2018-01-23 14:45:22 -0800

    Add tests for template

--- spec/models/template_spec.rb
+++ spec/models/template_spec.rb
@@ -35,28 +35,28 @@ RSpec.describe Template do
   end
 
   describe '#fields' do
-    describe 'each field' do
-      it 'has a label' do
-        subject.fields.each do |field|
-          expect(field['label']).to_not be_falsey
-        end
+     described_class.new('default').fields.each do |field|
+      it "#{field['name']} has a label" do
+        expect(field['label']).to_not be_falsey
       end
-      it 'has an example or default' do
-        subject.fields.each do |field|
-          expect(field['example'] || field['default']).to_not be_falsey
-        end
+      it "#{field['name']} has an example or default" do
+        expect(field['example'] || field['default']).to_not be_falsey
       end
-      it 'has a limit if not image' do
-        subject.fields.each do |field|
-          next if %w(ImageComponent SelectComponent).include? field['element']
-          next if field['type'] == 'color'
-          expect(field['limit']).to_not be_falsey
-        end
+      it "#{field['name']} has a limit if not image" do
+        next if %w(ImageComponent SelectComponent).include? field['element']
+        next if field['type'] == 'color'
+        expect(field['limit']).to_not be_falsey
       end
-      it 'has an element' do
-        subject.fields.each do |field|
-          expect(field['element']).to_not be_falsey
-        end
+      it "#{field['name']} default is not more than limit" do
+        next if %w(ImageComponent SelectComponent).include? field['element']
+        next if field['type'] == 'color'
+        next unless field['default']
+        default = field['default']
+        default = default.split('{').first + default.split('}').last if field['clear_brackets']
+        expect(field['limit'] > default.length).to be_truthy
+      end
+      it "#{field['name']} has an element" do
+        expect(field['element']).to_not be_falsey
       end
     end
   end
