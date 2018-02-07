133f1f349c831f82b5380a4ed50e2c204872c169
Scott Duncombe <sduncombe@renewfinancial.com>
2018-02-07 09:01:41 -0800

    Case insensitive eamils

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,13 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(access_token)
       data = access_token.info
-      user = User.where(email: data['email']).first
+      user = User.where("lower(email) = ?", data['email'].downcase).first
 
       unless user
         user = User.create!(first_name: data['first_name'],
                             last_name: data['last_name'],
                             pic: data['image'],
-                            email: data['email'],
+                            email: data['email'].downcase,
                             password: Devise.friendly_token[0,20])
 
         UserMailer.welcome(user).deliver_later
